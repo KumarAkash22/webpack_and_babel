c8390ef3550b4fd500143af6b9c58774596cea54
Akash Kumar Prasad <akash22gupta33@gmail.com>
2021-12-23 18:46:31 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import {add, multi} from "./calc.js";
-import("./index.css")
+import("./index.css");
+import reactimg from "./reactimg.png";
 
 console.log(add(3,5));
 
 console.log(multi(5,5));
 
 const h1 = document.createElement("h1");
-h1.innerText = "Hello Webpack "
+h1.innerText = "Hello Webpack";
+h1.classList.add("redcolor");
+const image = document.createElement("img");
+image.src= reactimg
 
-h1.classList.add("redcolor")
+document.getElementById("root").append(h1,image);
 
-document.getElementById("root").appendChild(h1);
+
+
+
