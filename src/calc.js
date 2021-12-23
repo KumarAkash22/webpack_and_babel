551a4a3c10164e111f93f665a952a7f6e1b84e0e
Akash Kumar Prasad <akash22gupta33@gmail.com>
2021-12-23 13:01:28 +0530

    Initial commit

---
+++ src/calc.js
@@ -0,0 +1,9 @@
+function add(first, second){
+    return first + second;
+}
+
+function multi(first, second){
+    return first * second;
+}
+
+export{add, multi}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {add, multi} from "./calc.js";
+
+console.log(add(3,5));
+
+console.log(multi(5,5));
